0e0af124fadc294bb9ab3a60b3c1ba28a842b0b3
Ajeet Gupta <229371738+gajit9147-dev@users.noreply.github.com>
2026-01-30 08:24:21 +0530

    All type using

--- stringinPython/escape_sequence.py
+++ stringinPython/escape_sequence.py
@@ -2,3 +2,16 @@
 #\t tab space
 #\\ backslash it gives backslash in output
 #\' single quote it gives single quote in output
+
+#\n
+print("Hello Ajeet!\nWelcome to Python programming.")
+
+#\t
+print("Hello Ajeet!\tWelcome to Python programming.")
+#\\
+print("This is backslash: \\")
+
+#\'
+
+print('It\'s a beautiful day.')
+print("It's a beautiful day.")
