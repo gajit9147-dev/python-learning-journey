b85535e8ddf44f0b2701a0e646f9ccdf78f62094
Ajeet Gupta <229371738+gajit9147-dev@users.noreply.github.com>
2026-01-30 08:50:10 +0530

    operation in string2

---
+++ stringinPython/operation_string2.py
@@ -0,0 +1,20 @@
+# count  syntax: string.count(substring, start= 0,end=len(string))
+
+st = "Ajeet Gupta is learning python. Ajeet is a good boy."
+print(st.count('a'))  #2
+print(st.count('A'))  #1
+s1= "Ajeet"
+
+print(f"counting of Ajeet in string: {st.count(s1)}")
+s2= " "
+print(f"counting of space in string: {st.count(s2)}")
+s3= "e"
+print(f"counting of e in string: {st.count(s3)}")
+
+#upper(), lower(), title(), capitalize()
+
+s4= "ajeet gupta is learning python."
+print(s4.upper())  # Converts to uppercase
+print(s4.lower())  # Converts to lowercase
+print(s4.title())  # Converts to title case
+print(s4.capitalize())  # Capitalizes the first character
