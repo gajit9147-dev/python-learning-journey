8027e226f4a87b540f415716daa98cdc3c72f3c1
Ajeet Gupta <229371738+gajit9147-dev@users.noreply.github.com>
2026-01-31 13:12:54 +0530

    list

---
+++ listinpython/list_operation.py
@@ -0,0 +1,7 @@
+# slicing list in python
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Get elements from index 2 to 5
+sliced_list = my_list[2:6]
+print(f"Sliced list from index 2 to 5: {sliced_list}")
+# Get every second element from the list
+print(my_list[1:6:1])
