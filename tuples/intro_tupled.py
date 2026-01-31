7c505d6135db15be5f8117bd96ba95865ea348f1
Ajeet Gupta <229371738+gajit9147-dev@users.noreply.github.com>
2026-01-31 15:05:08 +0530

    tuples

---
+++ tuples/intro_tupled.py
@@ -0,0 +1,9 @@
+# tuple is contain all type of data
+#in list  we can change the value 
+#it can't be changed (immutable)
+
+T1 =("Ajeet", 25, True, 5.9, 25000.50, [1,2,3], (4,5), {'name':'Ajeet'})
+print(T1)
+print(type(T1))
+print(T1[3])
+print(T1[-3])
