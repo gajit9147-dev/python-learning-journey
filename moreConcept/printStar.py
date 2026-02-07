3aafb167ceca083229c5fdd3e9b8656f39583e43
Ajeet Gupta <229371738+gajit9147-dev@users.noreply.github.com>
2026-02-08 00:54:41 +0530

    star

---
+++ moreConcept/printStar.py
@@ -0,0 +1,4 @@
+for i in range(1,7):
+    for j in range(1,i+2):
+        print("*", end="")  #end="" is used to print the next star in the same line
+    print()  #this is used to move to the next line after printing 5 stars in the current line
